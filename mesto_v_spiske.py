8c9a19d9ff8133bf229f10c75e4fb04211643da1
YegorCrazy <egor2002288@gmail.com>
2020-08-23 12:18:42 +0400

    Comments added

--- mesto_v_spiske.py
+++ mesto_v_spiske.py
@@ -12,19 +12,19 @@ with open('config.json', encoding="utf8") as config_file:
 
 link = config['link']
 
-site = requests.get(link)
+site = requests.get(link) #открываем сайт
 
-soup = BeautifulSoup(site.text, 'html.parser')
+soup = BeautifulSoup(site.text, 'html.parser') #читаем содержимое
 
 name = config['name']
 
-place = soup.find_all('td', text = name)
+place = soup.find_all('td', text = name) #ищем клетку по имени человека
 
 num = config['num']
 
-all_info = place[num-1].parent.find_all('td')
+all_info = place[num-1].parent.find_all('td') #находим строку-родитель клетки
 
-print(all_info[3].text)
+print(all_info[3].text) #выводим место
 
 a = input()
 
